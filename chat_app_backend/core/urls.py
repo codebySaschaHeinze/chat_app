30322a3c0e8464b16ddcf6256e6647eb7ad6290f
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-12 17:30:29 +0100

    refactor(core): Clean the code (deleted "home"-site for testing).

--- chat_app_backend/core/urls.py
+++ chat_app_backend/core/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("chat_app backend läuft")
 
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
     path("", include("chat.urls")),
 ]
