8adbd5a8c2443570c4f23edf21c54dd2da009df7
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-12 15:37:28 +0100

    feat(chat): add Chat model.

--- chat_app_backend/chat/models.py
+++ chat_app_backend/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Chat(models.Model):
+    name = models.CharField(max_length=30)
+    message = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.message[:30]}"
